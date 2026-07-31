c9e66461bfb8e2993c5ce5315716bc978c4b18e7
Sky637 <wilfredn888@gmail.com>
2026-07-31 22:17:12 +0800

    change back to correct firebase setting

--- acg-shop/src/firebase.ts
+++ acg-shop/src/firebase.ts
@@ -7,14 +7,13 @@ import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
 
 // 這些資訊來自你的 Firebase 控制台 (測試開發階段這些金鑰公開在外流是合法的)
 const firebaseConfig = {
-  apiKey: "AIzaSyAYXPLzk1uZw9iUIqzHKOruFU4eH7NeGK8",
-  authDomain: "web-test-81458.firebaseapp.com",
-  databaseURL: "https://web-test-81458-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "web-test-81458",
-  storageBucket: "web-test-81458.firebasestorage.app",
-  messagingSenderId: "370553168601",
-  appId: "1:370553168601:web:52c5f1d0a68c4782fc14c8",
-  measurementId: "G-DF03GDLLE1"
+  apiKey: "AIzaSyCiXm5gAs5Oktg4Jv-XS0R7bF2vsKz-894",
+  authDomain: "aliiance-studio-hk.firebaseapp.com",
+  projectId: "aliiance-studio-hk",
+  storageBucket: "aliiance-studio-hk.firebasestorage.app",
+  messagingSenderId: "547324701099",
+  appId: "1:547324701099:web:5dbdb1187220130ed24aee",
+  measurementId: "G-RJQJGGFC6T"
 };
 
 // 初始化 Firebase
